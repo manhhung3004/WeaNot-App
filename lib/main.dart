a613be8ceff574e6574495d5b11b5b6d98587e91
ManhHung <101622781+manhhung3004@users.noreply.github.com>
2023-10-18 20:14:44 +0700

    Get started

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,58 @@
-
 import 'package:flutter/material.dart';
-// import 'package:weather_app/homepage/widget_images.dart';
 
-void main() {
- runApp(const MyApp());
-}
+void main() => runApp( const MyApp());
 
-class MyApp extends StatelessWidget {
- const MyApp({super.key});
- @override
- Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: BackgroundImage(),
-      ),
-    );
- }
-}
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
-class BackgroundImage extends StatelessWidget {
- const BackgroundImage({super.key});
- @override
- Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          image: AssetImage(
-            "images/photo-1436891620584-47fd0e565afb1.png"),
-          fit: BoxFit.cover),
-      ),
-       child: const Stack(
-        children: [
-          CenteredHouse(),
-          temple()
-        ],
-       )
-    );
- }
-}
-
-class CenteredHouse extends StatelessWidget {
- const CenteredHouse({super.key});
- @override
- Widget build(BuildContext context) {
-    return Container(
-      width: 200,
-      height: 200,
-      decoration: const BoxDecoration(
-        image: DecorationImage(image: AssetImage("images/House.png"), fit: BoxFit.cover)
-      ),
-    );
- }
-}
-
-// ignore: camel_case_types
-class temple extends StatelessWidget {
-  const temple ({super.key});
-  
   @override
   Widget build(BuildContext context) {
-    
-    return Container(
-
+    return const MaterialApp( //use MaterialApp() widget like this
+      home: Home(), //create new widget class for this 'home' to 
+                   // escape 'No MediaQuery widget found' error
+      debugShowCheckedModeBanner: false,
     );
   }
 }
-  
+
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    
+    Size size = MediaQuery.of(context).size;
+
+    return Scaffold(
+      body: Container (
+        width: size.width,
+        height: size.height,
+        color: Colors.red,
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+
+            children: [
+              Image.asset('assets/get-started.png'),
+              const SizedBox(height: 30,),
+              Container(
+                height: 58,
+                width: size.width * 0.7,
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                  borderRadius: BorderRadius.all(Radius.circular(18)),
+                ),
+                child: const Center(
+                  child: Text('Get Started', style: TextStyle(color: Colors.white , fontSize: 18) )
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+

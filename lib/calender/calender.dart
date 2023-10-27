536be1f900bd7c5994bc541d953a42ec8a369187
mongquamonNT106 <21522125@gm.uit.edu.vn>
2023-10-28 02:25:52 +0700

    thêm chức năng calender

---
+++ lib/calender/calender.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:weather_app/main.dart';
+
+void main(){
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+class MyApp extends StatefulWidget {
+  const MyApp ({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  DateTime today = DateTime.now();
+  void _onDaySelected(DateTime day, DateTime focusedDay){
+    setState(() {
+      today = day;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Calender")),
+      body: content(),
+    );
+  }
+  Widget content(){
+    return Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          Text("Selected Day = " +today.toString().split("")[0]),
+          Container(
+            child: TableCalendar(
+              locale: "en_US",
+              rowHeight: 43,
+              headerStyle: HeaderStyle(formatButtonVisible: false,titleCentered: true),
+              availableGestures: AvailableGestures.all ,
+              selectedDayPredicate: (day)=>isSameDay(day,today),
+              focusedDay: today,
+              firstDay: DateTime.utc(2010, 10, 16),
+              lastDay: DateTime.utc(2030, 3, 14),
+              onDaySelected: _onDaySelected,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/sign_up.dart
+++ lib/ui/sign_up.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/my_sign_up_button.dart';
-import 'package:weather_app/text_fill.dart';
-import 'package:weather_app/my_button.dart';
+import 'package:weather_app/login/my_sign_up_button.dart';
+import 'package:weather_app/login/text_fill.dart';
+import 'package:weather_app/login/my_button.dart';
 
 
 class SignUp extends StatefulWidget {

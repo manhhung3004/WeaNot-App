efd09a0d3ceca9394961c09341821215f3df8afc
ManhHung <101622781+manhhung3004@users.noreply.github.com>
2023-10-27 14:32:11 +0700

    Revert "check code"
    This reverts commit 455b4b42b1c4a32aa92637f1ecbc6ca57e1f3127.

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/models/constants.dart';
 import 'package:weather_app/models/city.dart';
 import 'package:intl/intl.dart';
-import 'package:weather_app/ui/detail_page.dart';
 import 'package:weather_app/widgets/weather_item.dart';
 import 'package:http/http.dart' as http;
 import 'dart:core';
@@ -221,7 +220,7 @@ class _HomeState extends State<Home> {
                 ],
               ) ,
             ),
-            const SizedBox(height: 20,),
+            const SizedBox(height: 30,),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               crossAxisAlignment: CrossAxisAlignment.center,
@@ -263,13 +262,10 @@ class _HomeState extends State<Home> {
                 // ignore: non_constant_identifier_names
                 var SlipStringDate = DateFormat('HH, EEEE').format(parsedDate);
                 return GestureDetector(
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(consolidatedWeatherList: consolidataWeatherList , selectedId: index, location: location,)));
-                  },
                   child: Container(
                     padding:  const EdgeInsets.symmetric(vertical: 1),
                     margin: const EdgeInsets.only(right: 20 , bottom: 10 , top: 10),
-                    width: 110,
+                    width: 100,
                     decoration: BoxDecoration(
                       color: selectedDay == today ? myContants.primaryColor : Colors.white,
                       borderRadius: const BorderRadius.all(Radius.circular(10)),

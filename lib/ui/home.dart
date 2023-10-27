ddb7d677f04b88b6db8f00bb1f08c8a32fa4f2e1
ManhHung <101622781+manhhung3004@users.noreply.github.com>
2023-10-27 19:58:50 +0700

    detail page

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/models/constants.dart';
 import 'package:weather_app/models/city.dart';
 import 'package:intl/intl.dart';
+import 'package:weather_app/ui/detail_page.dart';
 import 'package:weather_app/widgets/weather_item.dart';
 import 'package:http/http.dart' as http;
 import 'dart:core';
@@ -253,10 +254,13 @@ class _HomeState extends State<Home> {
                 // ignore: non_constant_identifier_names
                 var SlipStringDate = DateFormat('HH, EEEE').format(parsedDate);
                 return GestureDetector(
+                  onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => DetailPage(consolidatedWeatherList: consolidataWeatherList, selectedId: index, location: location,)));
+                        },
                   child: Container(
                     padding:  const EdgeInsets.symmetric(vertical: 1),
                     margin: const EdgeInsets.only(right: 20 , bottom: 10 , top: 10),
-                    width: 100,
+                    width: 120,
                     decoration: BoxDecoration(
                       color: selectedDay == today ? myContants.primaryColor : Colors.white,
                       borderRadius: const BorderRadius.all(Radius.circular(10)),

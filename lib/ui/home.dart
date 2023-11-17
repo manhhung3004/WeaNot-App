049dab7897dd5bdd3b58e0645e35f9e38a3d084c
ManhHung <101622781+manhhung3004@users.noreply.github.com>
2023-11-17 14:47:27 +0700

    fixxx

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -59,7 +59,7 @@ class _HomeState extends State<Home> {
       //gán giá trị cho image url
       consolidataWeatherList = consolidatedWeather;
       imageUrl = weatherStateName.replaceAll(' ', '').toLowerCase();
-      consolidatedWeather = Set.from(result.values).toList();
+      // consolidatedWeather = Set.from(result.values).toList();
     });
   }
   @override
